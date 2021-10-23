0334a7d0edb3e759999ca0154c445aedad986771
susccrua <susccrua@gmail.com>
2021-10-23 14:30:34 -0500

    Added mapbox map with dummy markers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMapGL, { Marker } from "react-map-gl";
+import * as jesusData from "./data/jesus-life.json";
 
-function App() {
+export default function App() {
+
+  let [viewport, setViewport] = useState({
+    latitude: 31.7030,
+    longitude: 35.1955,
+    zoom: 1,
+    width: window.innerWidth,
+    height: window.innerHeight
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={"pk.eyJ1Ijoic2NydXo2OTM4IiwiYSI6ImNrdjN5d3AwcDNhMTkyd3A2dWtodnR5dnYifQ.W_HLl84L1OXwBiqbxnFvhg"}
+      mapStyle="mapbox://styles/scruz6938/ckv417krm0ih114pk04vfamus"
+      onViewportChange={(newView) => {
+        setViewport(newView);
+      }}
+    >
+
+        //dummy markers
+
+      {jesusData.features.map((jesusEvent) => (
+        <Marker key={jesusEvent.name} latitude={jesusEvent.geometry.coordinates[0]} longitude={jesusEvent.geometry.coordinates[1]}>
+          <div>{jesusEvent.name}</div>
+          <button>
+            <img src="/public/place.png" alt = "Location Icon"></img>
+          </button>
+          
+        </Marker>
+      ))}
+
+    </ReactMapGL>
   );
 }
-
-export default App;
