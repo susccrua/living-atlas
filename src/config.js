42fb5e33032691a7d9de6049df6c4f8572cf790d
susccrua <susccrua@gmail.com>
2021-10-24 12:40:27 -0500

    Added video from youtube

--- src/config.js
+++ src/config.js
@@ -12,11 +12,11 @@ var config = {
     chapters: [
         {
             id: '1',
-            alignment: 'left',
+            alignment: 'left-youtube',
             hidden: false,
             title: 'Birth',
-            image: './images/nativity.jpeg',
-            description: '<p>Jesus was born around 4 BC after Mary was told that she was to give birth by the Angel Gabriel. (Luke 1:26-28, Matthew 1:18-25) Mary was engaged to Joseph who is from the family of Judah and David. Mary then became pregnant by the Holy Spirit though this not the same as mythological gods impregnanting humans. This was required as it seems that the sinful nature was passed down through the father. This allowed Jesus to be fully human but not born into sin.* </p> <p><b>Dig deeper:</b><a href="https://www.gotquestions.org/virgin-birth.html">Why is the Virgin Birth so important?</a></p>',
+            //image: './images/nativity.jpeg',
+            description: '<iframe width="560" height="315" src="https://www.youtube.com/embed/_OLezoUvOEQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p>Jesus was born around 4 BC after Mary was told that she was to give birth by the Angel Gabriel. (Luke 1:26-28, Matthew 1:18-25) Mary was engaged to Joseph who is from the family of Judah and David. Mary then became pregnant by the Holy Spirit though this not the same as mythological gods impregnanting humans. This was required as it seems that the sinful nature was passed down through the father. This allowed Jesus to be fully human but not born into sin.* </p> <p><b>Dig deeper:</b><a href="https://www.gotquestions.org/virgin-birth.html">Why is the Virgin Birth so important?</a></p>',
             location: {
                 center: [35.1956, 31.7031],
                 zoom: 8.5,
